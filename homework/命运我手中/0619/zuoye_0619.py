9cf3b7f61dd55bb9f4223bf2a75bc83ea32cef0e
wangzhenhuaw <970786214@qq.com>
2019-06-20 23:21:06 +0800

    作业0617

---
+++ homework/命运我手中/0619/zuoye_0619.py
@@ -0,0 +1,25 @@
+# Date: 2019/6/20
+# Name: connor
+# _*_ conding:utf-8 _*_
+
+str1 = '1234567890'
+str2 = str1[:3]
+print(str2)
+str3 = str1[-3:]
+print(str3)
+str4 = str1[:]
+print(str4)
+str5 = str1[6:]
+print(str5)
+str6 = str1[:-3]
+print(str6)
+str7 = str1[2:3]
+print(str7)
+str8 = str1[-1:-2:-1]
+print(str8)
+str9 = str1[::-1]
+print(str9)
+str10 = str1[-3:-1]
+print(str10)
+str11 = str1[1:-1:2]
+print(str11)
